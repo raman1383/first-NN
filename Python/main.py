bc814d698e6bf86957e63a2c5b31434dfd121519
raman1383 <raman83shariati@gmail.com>
2022-12-16 15:37:34 +0330

    r

--- Python/main.py
+++ Python/main.py
@@ -23,7 +23,7 @@ class Activation_ReLU:
 
 class Activation_Softmax:
     def forward(self, inputs):
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True)) # to prevent overflow
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
