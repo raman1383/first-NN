6f0f62258a28907552a1885823b8852e9a11a19d
raman1383 <raman83shariati@gmail.com>
2022-12-16 11:21:11 +0330

    r

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,4 @@
-# import numpy as np
+import numpy as np
 
 inputs = [1, 2, 3, 2.5]
 
@@ -8,15 +8,15 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2.0, 3.0, 0.5]
 
-# output = np.dot(weights, inputs) + biases
+output = np.dot(weights, inputs) + biases
 
-layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input * weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
+# layer_outputs = []
+# for neuron_weights, neuron_bias in zip(weights, biases):
+#     neuron_output = 0
+#     for n_input, weight in zip(inputs, neuron_weights):
+#         neuron_output += n_input * weight
+#     neuron_output += neuron_bias
+#     layer_outputs.append(neuron_output)
+# print(layer_outputs)
 
-
-print(layer_outputs)
+print(output)
