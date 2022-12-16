89f747f829f931a3619fab25a6aeae3dded6dc1e
raman1383 <raman83shariati@gmail.com>
2022-12-16 10:28:47 +0330

    first NN

---
+++ Python/main.py
@@ -0,0 +1,11 @@
+# import numpy as np
+# import sys
+# import matplotlib
+
+input = [1, 2, 3]
+weights = [0.12, 0.8, 0.5]
+bias = 2
+
+output = input[0]*weights[0] + input[1]*weights[1]+input[2]*weights[2] + bias
+
+print(output)
