68792cb85e1cf9257ea1013be9e0a9bde61d6d81
raman1383 <raman83shariati@gmail.com>
2022-12-16 12:24:55 +0330

    r

--- Python/main.py
+++ Python/main.py
@@ -1,22 +1,26 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
 
-biases = [2.0, 3.0, 0.5]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-output = np.dot(weights, inputs) + biases
 
-# layer_outputs = []
-# for neuron_weights, neuron_bias in zip(weights, biases):
-#     neuron_output = 0
-#     for n_input, weight in zip(inputs, neuron_weights):
-#         neuron_output += n_input * weight
-#     neuron_output += neuron_bias
-#     layer_outputs.append(neuron_output)
-# print(layer_outputs)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.10 * np.random.rand(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-print(output)
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+# print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
