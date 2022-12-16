19a058d52e716ccdfe5e7307f3a41cd16404105e
raman1383 <raman83shariati@gmail.com>
2022-12-16 14:49:51 +0330

    r

--- Python/expo_norm.py
+++ Python/expo_norm.py
@@ -1,19 +1,18 @@
+"""Exponentiation + Normalization = Softmax"""
+
 import math
+
+import numpy
 
 """Exponentiation -> to remove negatives without losing meaning"""
 layer_outputs = [4.8, 1.21, 2.385]
 E = math.e
-exp_values = []
-for output in layer_outputs:
-    exp_values.append(E**output)
-print(exp_values)
+exp_values = numpy.exp(layer_outputs)
 
 
-"""normalization -> to avoid big numbers"""
+"""Normalization -> to avoid big numbers"""
+norm_values = exp_values / numpy.sum(exp_values)
 
-norm_base = sum(exp_values)
-norm_values = []
-for values in exp_values:
-    norm_values.append(values/norm_base)
+
 print(norm_values)
 print(sum(norm_values))
